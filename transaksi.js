c194ebc915844de2e565972e8fd78dd58e5e977d
gdp311201 <growspheredigpro@gmail.com>
2026-08-30 12:42:33 +0700

    Update transaksi.js

--- transaksi.js
+++ transaksi.js
@@ -1,4 +1,4 @@
-export const GAS_URL = "https://script.google.com/macros/s/AKfycbyNpKSzdj6K31lF-M60FP-KwZEuUXPmMBewXfxR9pmC4sYkWCZ1d9c0xqhzM5IpakgMsQ/exec";
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbye2uy5D3QALcS9W8xZb2twwnN0Bp4WTBCC-BrdGr_mmyeNvcr3g86JJChlSVMH39TFrQ/exec";
 
 export const SUB_DATA = {
   "💸 PINDAH DANA": ["SEABANK", "BCA", "MANDIRI", "DANA", "CASH"],
